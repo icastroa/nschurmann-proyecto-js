8d5c77fb347d622d9c5af585fa3d2fe4604e05f0
Isai <isaicastroa.99@gmail.com>
2023-04-05 11:14:36 -0600

    new

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fn = () => {
-    const miString = "soy un string!"
+const miString = "soy un string!"
     let b = miString + "muy feliz"
     console.log(b)
+    console.log('hola mundo');
+    
 }
 
 fn()
